1b67a0b5860330e9babff6d17d641b3074506a3c
mosstudio <daw_dash@mail.ru>
2021-09-05 17:05:49 +0500

    ab

--- app/src/main/java/gitexample.kt
+++ app/src/main/java/gitexample.kt
@@ -1,5 +1,12 @@
+import android.content.ContentValues.TAG
+import android.util.Log
+import android.util.Log.println
+
 //for git
 class git(){
     val gg="git begin"
     val fork="1f"
+    private fun p1() {
+        Log.d(TAG, "p1: ")
+    }
 }
